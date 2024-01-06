168ee61753a6c4da6caa08b7aaf4dfd6261962a9
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-01-06 15:49:49 +0200

    make build

---
+++ index.658f97be.js
@@ -0,0 +1,2 @@
+!function e(n,t){let r=document.createElement("ul");for(let l in t){let a=document.createElement("li");a.innerText=l,r.append(a),Object.keys(t[l]).length&&e(a,t[l]),n.append(r)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.658f97be.js.map
